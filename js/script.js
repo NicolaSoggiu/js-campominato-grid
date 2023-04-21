63f9bafa9074cba6b7d3d49bb56cea1475852822
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-04-21 18:51:26 +0200

    fix btn

--- js/script.js
+++ js/script.js
@@ -1,19 +1,20 @@
 const eleGrid = document.querySelector(".grid");
-let btnPlay = document.querySelector(".btn");
+const btnPlay = document.querySelector(".btn");
 
 btnPlay.addEventListener("click", function () {
   createGrid(100, eleGrid);
-  function createGrid(numCells, orderCells) {
-    for (let i = 1; i <= numCells; i++) {
-      const cell = document.createElement("div");
-      cell.classList.add("cell");
-      cell.innerHTML = [i];
-      orderCells.append(cell);
-      cell.addEventListener("click", function () {
-        console.log("cell", cell);
-        cell.classList.toggle("active");
-      });
-    }
-  }
 });
-debugger;
+
+function createGrid(numCells, eleContainer) {
+  eleContainer.innerHTML = "";
+  for (let i = 1; i <= numCells; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    cell.innerHTML = [i];
+    eleContainer.append(cell);
+    cell.addEventListener("click", function () {
+      console.log(cell);
+      cell.classList.toggle("active");
+    });
+  }
+}
