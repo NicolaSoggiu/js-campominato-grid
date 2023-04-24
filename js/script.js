f7d8195ab6c0c5b5ac5f13dc1a9f233a2b0e9555
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-04-24 11:40:09 +0200

    add html structure

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
+// DECLARE THE VARIABLES
 const eleGrid = document.querySelector(".grid");
 const btnPlay = document.querySelector(".btn");
 
+// BUTTON TO GENERATE THE GRID
 btnPlay.addEventListener("click", function () {
   createGrid(100, eleGrid);
 });
 
+// FUNCTINO TO CREATE THE GRID
 function createGrid(numCells, eleContainer) {
   eleContainer.innerHTML = "";
   for (let i = 1; i <= numCells; i++) {
@@ -12,6 +15,7 @@ function createGrid(numCells, eleContainer) {
     cell.classList.add("cell");
     cell.innerHTML = [i];
     eleContainer.append(cell);
+    // ADD COLOR WITH THE CLICK
     cell.addEventListener("click", function () {
       console.log(cell);
       cell.classList.toggle("active");
